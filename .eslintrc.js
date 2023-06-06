d7236055c4534490893e1705700f7774da21f677
Adarsh Pandey <108523569+Adarshpandey31@users.noreply.github.com>
2023-06-06 16:15:14 +0530

    fixed css for responsiveness of sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,13 @@ module.exports = {
     "es2021": true
     // "DISABLE_ESLINT_PLUGIN":true
   },
+
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
     "rallycoding"
   ],
+
   "parserOptions": {
     "ecmaVersion": "latest",
     "sourceType": "module",
@@ -38,38 +40,38 @@ module.exports = {
 "parser": "@babel/eslint-parser",
 
   "rules": {
-    "react/require-extension": "off",
+    // "react/require-extension": "off",
 
-    //React
-    'react/forbid-prop-types': 'error',
-    'react/no-multi-comp': ['error', { 'ignoreStateless': true }],
-    'react/no-set-state': 'error',
-    'react/no-string-refs': 'error',
-    'react/prefer-es6-class': 'error',
-    'react/prefer-stateless-function': 'error',
-    'react/require-extension': 'error',
-    'react/require-render-return': 'error',
-    'react/self-closing-comp': 'error',
-    'react/sort-comp': 'error',
-    'react/sort-prop-types': 'error',
-    'react/wrap-multilines': 'error',
+    // //React
+    // 'react/forbid-prop-types': 'error',
+    // 'react/no-multi-comp': ['error', { 'ignoreStateless': true }],
+    // 'react/no-set-state': 'error',
+    // 'react/no-string-refs': 'error',
+    // 'react/prefer-es6-class': 'error',
+    // 'react/prefer-stateless-function': 'error',
+    // 'react/require-extension': 'error',
+    // 'react/require-render-return': 'error',
+    // 'react/self-closing-comp': 'error',
+    // 'react/sort-comp': 'error',
+    // 'react/sort-prop-types': 'error',
+    // 'react/wrap-multilines': 'error',
 
-    // JSX
-    'react/jsx-boolean-value': 'error',
-    'react/jsx-closing-bracket-location': 'error',
-    'react/jsx-curly-spacing': ['error', 'always'],
-    'react/jsx-equals-spacing': 'error',
-    'react/jsx-first-prop-new-line': 'error',
-    'react/jsx-handler-names': 'error',
-    'react/jsx-indent-props': ['error', 2],
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-key': 'error',
-    'react/jsx-max-props-per-line': ['error', { 'maximum': 3 }],
-    'react/jsx-no-bind': 'error',
-    'react/jsx-no-literals': 'off',
-    'react/jsx-no-target-blank': 'error',
-    'react/jsx-pascal-case': 'error',
-    'react/jsx-sort-props': 'error',
-    'react/jsx-space-before-closing': 'error'
+    // // JSX
+    // 'react/jsx-boolean-value': 'error',
+    // 'react/jsx-closing-bracket-location': 'error',
+    // 'react/jsx-curly-spacing': ['error', 'always'],
+    // 'react/jsx-equals-spacing': 'error',
+    // 'react/jsx-first-prop-new-line': 'error',
+    // 'react/jsx-handler-names': 'error',
+    // 'react/jsx-indent-props': ['error', 2],
+    // 'react/jsx-indent': ['error', 2],
+    // 'react/jsx-key': 'error',
+    // 'react/jsx-max-props-per-line': ['error', { 'maximum': 3 }],
+    // 'react/jsx-no-bind': 'error',
+    // 'react/jsx-no-literals': 'off',
+    // 'react/jsx-no-target-blank': 'error',
+    // 'react/jsx-pascal-case': 'error',
+    // 'react/jsx-sort-props': 'error',
+    // 'react/jsx-space-before-closing': 'error'
   }
 };
